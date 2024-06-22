0ccd2f99796724e5c89257bd48bd2cb755dbc6a9
Echtaaa07 <atche120207@gmail.com>
2024-06-22 10:03:59 +0300

    initial commit 3

--- src/main/java/com/example/spring/boot/API/SpringBootApiApplication.java
+++ src/main/java/com/example/spring/boot/API/SpringBootApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Product API", description = "product microservice"))
+@OpenAPIDefinition(info = @Info(title = "Product API", description = "product microservicee"))
 public class SpringBootApiApplication {
 
 	public static void main(String[] args) {
